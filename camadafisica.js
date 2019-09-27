bfa0e8ae3372a49922472c15d9cbf5d87fbec2a8
renanpaiva <renan0paiva@hotmail.com>
2019-09-27 18:56:04 -0300

    camada física

--- camadafisica.js
+++ camadafisica.js
@@ -4,15 +4,20 @@ function newCamadaFisica(meio){
 var obj;
 
 obj.isMeioOcupado= function(){
-
+   return meiosFisicos[meio].ocupado;
 };
 
 obj.Transmitir = function(data){
-
+if(meiosFisicos[meio].ocupado){
+    meiosFisicos[meio].colisao=true;
+}else{
+    meiosFisicos[meio].transmissao=data;
+    meiosFisicos[meio].ocupado=true;
+}
 };
 
 obj.Receber= function(){
-
+ return meiosFisicos[meio].transmissao
 }
 return obj;
 };
