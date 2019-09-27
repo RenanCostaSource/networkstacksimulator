f353735de4051ec193310c9fb7fef431cd5f7d5c
renanpaiva <renan0paiva@hotmail.com>
2019-09-27 19:00:30 -0300

    randomiza ordem dos computadores

--- mundo.js
+++ mundo.js
@@ -16,7 +16,29 @@ function init(){
 
 
 function step(){
+    computers= shuffle(computers);//randomiza a ordem dos computadores
     for(var i=0;i<computers.length;i++){
 
     }
 };
+
+
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
